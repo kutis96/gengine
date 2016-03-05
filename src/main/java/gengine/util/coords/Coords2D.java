c92591ec545de1bd2cabf0d722bfd3b022abe9d0
Richard Kutina <kutinric@fel.cvut.cz>
2016-03-05 18:45:22 +0100

    Fixed some facepalming little issues with the Coords2D/3D

--- src/main/java/gengine/util/coords/Coords2D.java
+++ src/main/java/gengine/util/coords/Coords2D.java
@@ -1,32 +1,61 @@
 package gengine.util.coords;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Coordinates to specify position in 2D worlds with integer coordinates.
+ * Coordinates to specify position in 2D worlds.
  *
  * @author Richard Kutina <kutinric@fel.cvut.cz>
  */
 public class Coords2D extends CoordFixedD {
 
-    private float[] pos;
-
+    /**
+     * Constructs the Coords2D with default value of (0, 0).
+     */
     public Coords2D() {
         super(2);
+        
+        try {
+            this.setCoords(new float[]{0, 0});
+        } catch (DimMismatchException ex) {
+            //This should actually never happen
+            Logger.getLogger(Coords3D.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
+    /**
+     * Constructs the Coords2D with a given default value
+     *
+     * @param x default x-coordinate
+     * @param y default y-coordinate
+     */
     public Coords2D(float x, float y) {
         super(2);
-        this.pos = new float[]{x, y};
+        
+        try {
+            this.setCoords(new float[]{x, y});
+        } catch (DimMismatchException ex) {
+            //This should actually never happen
+            Logger.getLogger(Coords3D.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
+    /**
+     * Gets the X-coordinate
+     *
+     * @return the X-coordinate
+     */
     public float getX() {
-        return pos[0];
+        return this.getCoords()[0];
     }
 
+    /**
+     * Gets the Y-coordinate
+     *
+     * @return the Y-coordinate
+     */
     public float getY() {
-        return pos[1];
-    }
-
-    public float getZ() {
-        return pos[2];
+        return this.getCoords()[1];
     }
 }
--- src/main/java/gengine/util/coords/Coords3D.java
+++ src/main/java/gengine/util/coords/Coords3D.java
@@ -1,32 +1,71 @@
 package gengine.util.coords;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Coordinates to specify position in 3D worlds with integer coordinates.
+ * Coordinates to specify position in 3D worlds.
  *
  * @author Richard Kutina <kutinric@fel.cvut.cz>
  */
 public class Coords3D extends CoordFixedD {
 
-    private float[] pos;
-
+    /**
+     * Constructs the Coords2D with a default value of (0, 0, 0).
+     */
     public Coords3D() {
         super(3);
+        
+        try {
+            this.setCoords(new float[]{0, 0, 0});
+        } catch (DimMismatchException ex) {
+            //This should actually never happen
+            Logger.getLogger(Coords3D.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
+    /**
+     * Constructs the Coords2D with a given default value
+     *
+     * @param x default x-coordinate
+     * @param y default y-coordinate
+     * @param z default z-coordinate
+     */
     public Coords3D(float x, float y, float z) {
         super(3);
-        this.pos = new float[]{x, y, z};
+        
+        try {
+            this.setCoords(new float[]{x, y, z});
+        } catch (DimMismatchException ex) {
+            //This should actually never happen
+            Logger.getLogger(Coords3D.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
+    /**
+     * Gets the X-coordinate
+     *
+     * @return the X-coordinate
+     */
     public float getX() {
-        return pos[0];
+        return this.getCoords()[0];
     }
 
+    /**
+     * Gets the Y-coordinate
+     *
+     * @return the Y-coordinate
+     */
     public float getY() {
-        return pos[1];
+        return this.getCoords()[1];
     }
 
+    /**
+     * Gets the Z-coordinate
+     *
+     * @return the Z-coordinate
+     */
     public float getZ() {
-        return pos[2];
+        return this.getCoords()[2];
     }
 }
